42420f1b6f38850aaf5e4ca57e8a01525df011b6
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-22 20:22:17 -0700

    (feat) Implemented Forever, a never-dropping data store.

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+#![license = "MIT"]
+#![deny(missing_doc)]
+#![deny(warnings)]
+
+#![feature(unsafe_destructor)]
+
+//! Shareable data that lasts forever, with no reference count.
+
+use std::mem;
+
+/// Shareable data that lasts forever, with no reference count.
+#[unsafe_no_drop_flag]
+pub struct Forever<T> {
+    __data: *mut T
+}
+
+impl<T: Send + Sync> Forever<T> {
+    /// Create a new, never-dropping, wrapper around T.
+    ///
+    /// This means T will *never* be deallocated and can be
+    /// the source of memory leaks if you are not careful
+    /// about when you create a Forever.
+    ///
+    /// For that reason, this function is marked unsafe.
+    #[inline]
+    pub unsafe fn new(val: T) -> Forever<T> {
+        Forever { __data: mem::transmute(box val) }
+    }
+
+    /// Get an immutable reference to the contents of a Forever.
+    ///
+    /// This is safe because the underlying data is Send + Sync
+    /// anyway.
+    #[inline]
+    pub fn inner(&self) -> &T {
+        unsafe { mem::transmute(self.__data) }
+    }
+}
+
+impl<T: Send + Sync> Clone for Forever<T> {
+    #[inline]
+    fn clone(&self) -> Forever<T> {
+        Forever { __data: self.__data }
+    }
+}
+
+impl<T: Send + Sync> Deref<T> for Forever<T> {
+    #[inline]
+    fn deref(&self) -> &T {
+        self.inner()
+    }
+}
+
+#[unsafe_destructor]
+impl<T: Sync + Send> Drop for Forever<T> {
+    // Dropping this does nothing.
+    // This can be the source of memory leaks if you are not careful!
+    #[inline]
+    fn drop(&mut self) {}
+}
+
